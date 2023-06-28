6358ca296b8d03f2d96cd7399bd7fa3007dbd301
Vijay <info@gmail.com>
2023-06-28 16:07:14 +0530

    comment add

--- demo/src/main/java/com/helloword/App.java
+++ demo/src/main/java/com/helloword/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-		System.out.println("Check the GitHub Action");
+		System.out.println("Check the GitHub Action play");
     }
 }
